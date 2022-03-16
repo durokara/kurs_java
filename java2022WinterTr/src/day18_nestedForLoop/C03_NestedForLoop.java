04a9abbac8aadc8dbbd19006cb43d9f71c8a2403
duro <d.erkaraman0666@gmail.com>
2022-03-16 22:15:40 +0100

    16.03.2022

--- java2022WinterTr/src/day18_nestedForLoop/C03_NestedForLoop.java
+++ java2022WinterTr/src/day18_nestedForLoop/C03_NestedForLoop.java
@@ -6,6 +6,8 @@ public class C03_NestedForLoop {
         //      * *
         //      * * *
         //      * * * *
+        //      * * * * *
+        
         int input = 5;
         for (int i = 1; i <=input ; i++) {
             for (int j = 1; j <=i ; j++) {
