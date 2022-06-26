65e514b6aae8e9bb7c5e48050f9714b94378c691
duro <d.erkaraman0666@gmail.com>
2022-06-26 22:35:50 +0200

    26.06.2022

---
+++ java2022WinterTr/src/day54/TC_01_SatisRaporlari.java
@@ -0,0 +1,15 @@
+package day54;
+
+
+
+public class TC_01_SatisRaporlari {
+
+   /*
+
+        8-"Yil" sekmesine tiklar ve yillik satis raporlarini görür
+        9-"Son Ay" sekmesine tiklar ve son Ay icin satis raporlarini görür
+        10-"Bu Ay" sekmesine tiklar ve icinde bulunulan Ay'in satis raporlarini görür
+        11-"Son Yedi Gün" sekmesine tiklar ve son yedi gün icin satis raporlarini görür
+    */
+
+   }
