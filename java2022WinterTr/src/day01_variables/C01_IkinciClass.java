82330f68547e624971a038ef1ec4222dc19adea1
duro <d.erkaraman0666@gmail.com>
2023-03-01 19:07:42 +0100

    01.03.2023

---
+++ java2022WinterTr/src/day01_variables/C01_IkinciClass.java
@@ -0,0 +1,29 @@
+package day01_variables;
+
+public class C01_IkinciClass {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World yazdiran javayi halleder");
+
+        int level=1;
+
+        System.out.println(level);
+
+        boolean ogrencimi= true;
+        boolean yagisvarmi= false;
+        //sadece dogru ve yanlis icin kullanilir
+
+
+        char ozelsembol='a';
+        char sayi='2';
+        //sadece tek bir karakter icin kullanilir
+
+
+        //bir variable olustururken icine koyacagimiz datanin alabilecegi degerlere uygun bir data türü secmeliyiz
+        //ornegin bir sehrin nufusundan bahswediyorsak
+        //variable mizin data turu String,boolean, char veya double olamaz
+        //biz kurs boyunca tam sayilar icin int, ondalikli sayilar icin double kullanacagiz
+
+
+    }
+}
